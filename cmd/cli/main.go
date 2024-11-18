ee16e3cd5e8cb587b258952a62c1bff2cb8bcbb3
Cuong Tran <cuong@silentium.io>
2024-11-18 16:56:20 +0700

    feat: graceful shutdown fx

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"context"
+	"os"
+	"syscall"
+	"time"
 
 	"github.com/cuongtranba/mynoti/internal/appfx"
+	"github.com/cuongtranba/mynoti/pkg/app_context"
+	"github.com/cuongtranba/mynoti/pkg/signal"
 )
 
 func main() {
-	if err := appfx.CLIApp.Start(context.Background()); err != nil {
+	app := appfx.NewFxRunner(appfx.CLIApp)
+	defer app.Stop(app_context.New(context.Background()))
+	err := signal.Run(
+		app_context.New(context.Background()),
+		app,
+		3*time.Second,
+		syscall.SIGINT,
+		os.Interrupt,
+		os.Kill,
+	)
+	if err != nil {
 		panic(err)
 	}
 }
